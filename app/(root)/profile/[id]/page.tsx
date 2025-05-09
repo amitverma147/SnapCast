5b4acf79d3d203506bed3f0018763f7f4be07a10
Amit Verma <vamit20000@gmail.com>
2025-05-10 02:48:46 +0530

    navbar and header done partially

---
+++ app/(root)/profile/[id]/page.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/Header";
+
+const page = async ({ params }: ParamsWithSearch) => {
+  const { id } = await params;
+  return (
+    <div className='wrapper page'>
+      <Header
+        subHeader="amitv147@gmail.com"
+        title="Amit"
+        userImg="/assets/images/dummy.jpg"
+      />
+      <h1 className="text-2xl font-karla">User ID: {id}</h1>
+    </div>
+  );
+};
+
+export default page;
